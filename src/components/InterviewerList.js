356f7fd6805f12b521ef8fdb9a75fa3ed1805c6a
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-12-08 17:27:09 -0500

    Finished building InterviewList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import "components/InterviewerList";
+import "components/InterviewerList.scss";
 import InterviewerListItem from './InterviewerListItem';
 
 
 export default function InterviewerList(props) {
   const { interviewers, setInterviewer, interviewer } = props;
   
-  const InterviewerList = interviewers.map((item) => {
-    return <InterviewerListItem
-    key={item.id}
+  const interviewerList = interviewers.map((item) => {
+    return <InterviewerListItem key={item.id}
     name={item.name}
     avatar={item.avatar}
-    selected={item.name === interviewer}
+    selected={item.id === interviewer}
     setInterviewer={setInterviewer}
+    id={item.id}
     />
   })
-console.log("interviewers ---> ", interviewer)
+  
+// console.log("interviewers ---> ", interviewer)
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {InterviewerList}
+        {interviewerList}
       </ul>
     </section>
   );
