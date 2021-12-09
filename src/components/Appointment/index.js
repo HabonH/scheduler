4ea1a36f13a9c51304715e7ef53ce2ec851d5917
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-12-09 16:24:11 -0500

    Added header, empty, show to Appointment w/ Fragments & conditionals

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import "components/Appointment/styles.scss"
+import React, { Fragment } from 'react';
+import "components/Appointment/styles.scss";
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
-
+//How does Appointment have access to the values of children components
+// Once I deconstruct props, will React know that I want to use the props of Appt to get the prop values in the child component?
 export default function Appointment(props) {
-  const { time } = props;
+  const { id, time, interview } = props;
+  console.log("Interview ---> ", interview)
   return (
     <article className="appointment">
-      {time ? `Appointment at ${time}` : `No Appointments`}
+      <Fragment>
+        {interview ?
+          <>
+            <Header time={time} />
+            <Show
+              id={id}
+              student={interview.student} 
+              interviewer={interview.interviewer.name} />
+          </>
+          :
+          <>
+            <Header time={time} />
+            <Empty />
+
+          </>
+        }
+      </Fragment>
     </article>
   );
 }
