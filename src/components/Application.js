39fde3d7e2ae8e0ece86237057278643b437bea1
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-12-09 23:38:36 -0500

    added axios api and use effect to Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
+import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -67,7 +51,19 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState([]);
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days")
+      .then((response) => {
+        setDays([response])
+        console.log("response ---> ", response);
+
+      });
+
+  }, []);
+      
 
   return (
     <main className="layout">
